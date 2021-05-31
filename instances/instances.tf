b404c3f0edda610d31d1ad943172c817ea7ae3f4
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-05-31 16:08:28 -0300

    29. Defining Backend and Reading Remote State for Layer 1 Infrastructure

---
+++ instances/instances.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "network_configuration" {
+  backend = "s3"
+  config {
+    bucket    = "${var.remote_state_bucket}"
+    key       = "${var.terraform_state_key}"
+    region    = "${var.region}"
+  }
+}
