f16827adab527d59e066dc0104bad0f9da3a6a7c
Dherkyan Ribeiro <dherkyan@gmail.com>
2021-06-01 00:31:20 -0300

    30. Creating Security Groups for EC2 Instances and ELB

--- instances/instances.tf
+++ instances/instances.tf
@@ -14,3 +14,79 @@ data "terraform_remote_state" "network_configuration" {
     region    = "${var.region}"
   }
 }
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name = "EC2-Public-SG"
+  description = "Internet readching access for EC2 Instances"
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "TCP"
+    to_port = 22
+    cidr_blocks = ["170.246.97.189"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_private_security_group" {
+  name = "EC2-Private-SG"
+  description = "Only allow public SG resources to access these instances"
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["${aws_security_group.ec2_public_security_group.id}"]
+  }
+
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow health checking for instances using this SG"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name = "ELB-SG"
+  description = "ELB Security Group"
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow web tracffic to load balancer"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
